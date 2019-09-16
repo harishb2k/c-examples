eead54cce596df255a62808ab27e9ceae4e99f15
Harish Singh Bohara <harish.bohara@olacabs.com>
2019-09-16 14:45:46 +0530

    Added rocks Db support

--- sub-dir-example/src/main.cpp
+++ sub-dir-example/src/main.cpp
@@ -6,9 +6,16 @@
 #include <iostream>
 #include <helper/helper.h>
 #include <cppkafka/cppkafka.h>
+#include "rocksdb/db.h"
+#include <assert.h>
 
 using namespace std;
 using namespace cppkafka;
+using namespace rocksdb;
+
+std::string kDBPath = "/tmp/rocksdb_simple_example";
+
+void main_rocks();
 
 int main() {
     cout << "Sub Dir Example " << endl;
@@ -20,7 +27,7 @@ int main() {
 
     // Create the config
     Configuration config = {
-            { "metadata.broker.list", "127.0.0.1:9092" }
+            {"metadata.broker.list", "127.0.0.1:9092"}
     };
 
     // Create the producer
@@ -28,8 +35,38 @@ int main() {
 
     // Produce a message!
     string message = "hey there!";
-    producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
-    producer.flush();
+    for (int i = 0; i < 1; i++) {
+        // producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
+
+        std::string text = "Hi";
+        text += std::to_string(i);
+        producer.produce(MessageBuilder("my_topic").payload(text));
+        producer.flush();
+    }
+
+
+    main_rocks();
 
     return 0;
+}
+
+
+void main_rocks() {
+    rocksdb::DB *db;
+    Status status;
+
+    rocksdb::Options options;
+    options.create_if_missing = true;
+
+    status = DB::Open(options, kDBPath, &db);
+    assert(status.ok());
+
+    status = db->Put(WriteOptions(), Slice("key"), Slice("value-1"));
+    assert(status.ok());
+
+    std::string value;
+    status = db->Get(ReadOptions(), Slice("key"), &value);
+    cout << value << endl;
+
+    delete db;
 }
