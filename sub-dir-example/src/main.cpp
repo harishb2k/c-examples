ea36e2b4e2e42c12b62c3fd978783a720262e2ff
Harish Singh Bohara <harish.bohara@olacabs.com>
2019-09-16 12:01:24 +0530

    Updated cmake to have kafka support

--- sub-dir-example/src/main.cpp
+++ sub-dir-example/src/main.cpp
@@ -5,14 +5,31 @@
 #include "main.h"
 #include <iostream>
 #include <helper/helper.h>
+#include <cppkafka/cppkafka.h>
 
 using namespace std;
+using namespace cppkafka;
+
 int main() {
     cout << "Sub Dir Example " << endl;
 
 
     Helper helper;
     helper.printHelp();
+
+
+    // Create the config
+    Configuration config = {
+            { "metadata.broker.list", "127.0.0.1:9092" }
+    };
+
+    // Create the producer
+    Producer producer(config);
+
+    // Produce a message!
+    string message = "hey there!";
+    producer.produce(MessageBuilder("my_topic").partition(0).payload(message));
+    producer.flush();
 
     return 0;
 }
